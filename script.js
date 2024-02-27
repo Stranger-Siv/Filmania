49725782bb3c1193eae437c7320f6d5da567ca26
Sivram Pradhan <programmersiv21@gmail.com>
2024-02-27 16:28:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ var popular = [
 	{ name: "Recline Chair", headline: "Nice as Fuck", price: "25000", image:"https://images.unsplash.com/photo-1580480055273-228ff5388ef8?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
 	{ name: "Royal Chair", headline: "So Cool", price: "55000", image:"https://images.unsplash.com/photo-1517705008128-361805f42e86?q=80&w=2887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
 ]
-
 var cart=[]
-
 function addProduct() {
 	var clutter = "";
 	products.forEach(function(product,index){
